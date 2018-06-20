c4295948d03ad2deff6255cfaf53fe97e2bf98d4
Arzoo Nagpal <AN00347574@TechMahindra.com>
2018-06-20 15:50:12 +0530

    sample change

--- featureRequestAppEnv/App/__init__.py
+++ featureRequestAppEnv/App/__init__.py
@@ -4,4 +4,4 @@ from flask import Flask
 # After importing Flask, we need to create an instance of the Flask class for our web application. Passing __name__ is going to configure flask.
 app= Flask(__name__)
 #importing views.py, FeatureRequestManager.py, featureAppServices.py, featureAppModel.py
-from App import views,FeatureRequestManager,featureRequestModel,featureRequestServices,config
+from App import views,FeatureRequestManager,featureRequestModel,featureRequestServices,config 
